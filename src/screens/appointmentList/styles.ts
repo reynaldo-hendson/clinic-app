f01a2630b3a9c30501360da20bb5f1359102293f
Reynaldo Hendson <reynaldohendsondev@outlook.com>
2025-06-19 10:42:51 -0300

    feat: adiciona screen que lista todas as sessões de uma determinada data

---
+++ src/screens/appointmentList/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#f2f2f2'
+    },
+    datePicker: {
+      marginTop:20
+    },
+    card: {
+        backgroundColor: '#fff',
+        padding: 16,
+        borderRadius: 8,
+        marginBottom: 12,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 4
+    },
+    list:{
+        flex: 1,
+        marginTop: 35
+    }
+
+});
